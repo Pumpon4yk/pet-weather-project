ccc86bebd2fd829069b9a6f2d759efefae1224b6
Pumpon4yk <tt10392@gmail.com>
2023-05-03 22:43:42 +0200

    beta card

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,10 @@
+import CardWeater from '../CardWeater/CardWeater';
 import { Container} from './Home.styled';
 
 const Home = () => {
   return (
 <Container>
+<CardWeater/>
 </Container>
   );
 };
