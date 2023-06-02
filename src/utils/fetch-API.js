06a8954619f35958ff28530b141900168c8892f1
Pumpon4yk <tt10392@gmail.com>
2023-06-02 21:45:14 +0200

    fix key api

--- src/utils/fetch-API.js
+++ src/utils/fetch-API.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API_WEATHER_KEY  =" 9fe2f0211b2345f30e9aa566a923a403";
+const API_WEATHER_KEY  ="9fe2f0211b2345f30e9aa566a923a403";
 const BASE_URL = 'https://api.openweathermap.org/data/2.5'
 
 
