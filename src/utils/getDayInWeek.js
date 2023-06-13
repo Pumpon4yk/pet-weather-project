fc9c2a0319d3778795e6180184b693707cd4d836
Pumpon4yk <tt10392@gmail.com>
2023-06-13 23:08:47 +0200

    change func getDayWeek

--- src/utils/getDayInWeek.js
+++ src/utils/getDayInWeek.js
@@ -1,7 +1,16 @@
 export const getDayInWeek = (dateString) => {
   const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const date = new Date(dateString);
-  const dayOfWeekIndex = date.getDay();
-  const dayOfWeek = daysOfWeek[dayOfWeekIndex];
-  return dayOfWeek;
+  const today = new Date(); // Поточна дата
+  const tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1); // Завтрашня дата
+
+  if (date.toDateString() === today.toDateString()) {
+    return "Today";
+  } else if (date.toDateString() === tomorrow.toDateString()) {
+    return "Tomorrow";
+  } else {
+    const dayOfWeekIndex = date.getDay();
+    const dayOfWeek = daysOfWeek[dayOfWeekIndex];
+    return dayOfWeek;
+  }
 }
